fe4bb8dca61a8a2b7fe0518e5e8a050888771986
JMartins <jorge.martins2323@gmail.com>
2022-01-14 21:40:57 +0000

    First Commit
    Inserção da Instalação do Laravel mais o layout realizado como teste do nonio para os colegas da pagina principal.

---
+++ laravel/resources/views/welcome.blade.php
@@ -0,0 +1,489 @@
+@extends('layout')
+
+@section('title', 'Home')
+
+@section('content')
+
+<table class="page">
+			
+			
+    <tbody><tr>
+        <td>
+            <table class="zone">
+                <tbody><tr>
+                    <td>
+                        <table class="horizontalline">
+                            <tbody><tr>
+                                <td class="subtitle">
+                                    <a class="handMouseCursor" href="javascript: expandSection('div_AulasRecentes')" id="link">
+                                          <span style="display: inline;" id="div_AulasRecentesPlus">
+                                              <img src="../images/setaBaixo.png?v=1639596198513">
+                                          </span>&nbsp;			  												
+                                        Aulas Recentes							
+                                    </a>
+                                </td>
+                            </tr>									
+                        </tbody></table>
+                    </td>		
+                </tr>
+                <tr>
+                    <td>
+                        <table class="zonecontent">
+                            <tbody><tr>
+                                <td>
+                                    <div style="display: none" id="div_AulasRecentes">	
+
+<table class="tabledefault">
+
+<tbody><tr>
+<td style="width:100%;">
+    <table class="darkrow" style="width:100%;border-top:0;border-bottom:1px solid #ccc;">
+        <tbody><tr class="cellheader">
+            
+                
+                    <td style="width:8%;text-align:left;"><strong>Turma</strong></td>
+                
+                
+            
+            <td style="width:10%;text-align:left;"><strong>Data</strong></td>												
+            <td style="width:10%;text-align:left;"><strong>Hora de Início</strong></td>
+            <td style="width:10%;text-align:left;"><strong>Duração</strong></td>
+            
+            
+                <td style="width:10%;text-align:left;"><strong>Modo de Leccionação</strong></td>
+            
+            
+            <td style="width:40%;text-align:left;"><strong>Docentes</strong></td>
+            <td style="width:13%;text-align:left;"><strong>Sala</strong></td>
+        </tr>
+        <tr>
+            
+                
+                    <td style="width:8%; text-align:left;">T11</td>
+                
+                
+            
+            
+            <td style="width:10%; text-align:left;">2021-12-13</td>
+            <td style="width:10%; text-align:left;">11:00</td>
+            <td style="width:10%; text-align:left;">2h</td>
+            
+            
+                <td style="width:10%; text-align:left;">
+                    
+                </td>
+            
+            
+            <td style="width:40%; vertical-valign:top;">
+            
+                
+                    
+                    
+                        
+                            Pedro Manuel Espírito Santo
+                            <br>
+                        
+                    
+                
+            </td>	
+            <td style="width:13%; text-align:left;">
+                
+                    
+                        
+                            Ed. Principal - S2.12
+                        
+                    
+                    
+                
+            </td>
+        </tr>
+        
+            
+            
+            
+                <tr>                                                            	
+                    <td colspan="6">	
+                        <table class="tabledefault">
+                            
+                                
+                                    
+                                    
+                                        
+                                            <tbody><tr>
+                                              <td class="label"><p>Português:</p></td>
+                                              <td class="cellcontent"><p><span>Definição de objetivos e indicadores estratégicos para plano de e-marketing</span></p>
+<p>Criação de contas nas plataformas necessárias para:</p>
+<p><em>- criação do website (Wordpress), </em></p>
+<p><em>- gestão redes sociais (Facebook e instagram, Youtube)</em></p>
+<p><em>- E-mail marketing (mailchimp)</em></p>
+<p>Estruturar os conteúdos a publicar:</p>
+<p><em>- Estrutura e Mapa do website</em></p></td>
+                                            </tr>
+                                        
+                                            <tr>
+                                              <td class="label"><p>Inglês:</p></td>
+                                              <td class="cellcontent"></td>
+                                            </tr>
+                                        
+                                                
+                                        
+                            
+                        </tbody></table>
+                    </td>
+                </tr>
+            
+        
+    </tbody></table>
+</td>
+</tr>
+
+<tr>
+<td style="width:100%;">
+    <table class="darkrow" style="width:100%;border-top:0;border-bottom:1px solid #ccc;">
+        <tbody><tr class="cellheader">
+            
+                
+                    <td style="width:8%;text-align:left;"><strong>Turma</strong></td>
+                
+                
+            
+            <td style="width:10%;text-align:left;"><strong>Data</strong></td>												
+            <td style="width:10%;text-align:left;"><strong>Hora de Início</strong></td>
+            <td style="width:10%;text-align:left;"><strong>Duração</strong></td>
+            
+            
+                <td style="width:10%;text-align:left;"><strong>Modo de Leccionação</strong></td>
+            
+            
+            <td style="width:40%;text-align:left;"><strong>Docentes</strong></td>
+            <td style="width:13%;text-align:left;"><strong>Sala</strong></td>
+        </tr>
+        <tr>
+            
+                
+                    <td style="width:8%; text-align:left;">T11</td>
+                
+                
+            
+            
+            <td style="width:10%; text-align:left;">2021-12-13</td>
+            <td style="width:10%; text-align:left;">09:00</td>
+            <td style="width:10%; text-align:left;">2h</td>
+            
+            
+                <td style="width:10%; text-align:left;">
+                    
+                </td>
+            
+            
+            <td style="width:40%; vertical-valign:top;">
+            
+                
+                    
+                    
+                        
+                            Pedro Manuel Espírito Santo
+                            <br>
+                        
+                    
+                
+            </td>	
+            <td style="width:13%; text-align:left;">
+                
+                    
+                        
+                            Ed. Principal - S2.12
+                        
+                    
+                    
+                
+            </td>
+        </tr>
+        
+            
+            
+            
+                <tr>                                                            	
+                    <td colspan="6">	
+                        <table class="tabledefault">
+                            
+                                
+                                    
+                                    
+                                        
+                                            <tbody><tr>
+                                              <td class="label"><p>Português:</p></td>
+                                              <td class="cellcontent"><p>Apresentação do trabalho de grupo</p>
+<p>Constituição dos grupos</p></td>
+                                            </tr>
+                                        
+                                            <tr>
+                                              <td class="label"><p>Inglês:</p></td>
+                                              <td class="cellcontent"></td>
+                                            </tr>
+                                        
+                                                
+                                        
+                            
+                        </tbody></table>
+                    </td>
+                </tr>
+            
+        
+    </tbody></table>
+</td>
+</tr>
+
+<tr>
+<td style="width:100%;">
+    <table class="darkrow" style="width:100%;border-top:0;border-bottom:1px solid #ccc;">
+        <tbody><tr class="cellheader">
+            
+                
+                    <td style="width:8%;text-align:left;"><strong>Turma</strong></td>
+                
+                
+            
+            <td style="width:10%;text-align:left;"><strong>Data</strong></td>												
+            <td style="width:10%;text-align:left;"><strong>Hora de Início</strong></td>
+            <td style="width:10%;text-align:left;"><strong>Duração</strong></td>
+            
+            
+                <td style="width:10%;text-align:left;"><strong>Modo de Leccionação</strong></td>
+            
+            
+            <td style="width:40%;text-align:left;"><strong>Docentes</strong></td>
+            <td style="width:13%;text-align:left;"><strong>Sala</strong></td>
+        </tr>
+        <tr>
+            
+                
+                    <td style="width:8%; text-align:left;">T11</td>
+                
+                
+            
+            
+            <td style="width:10%; text-align:left;">2021-12-06</td>
+            <td style="width:10%; text-align:left;">11:00</td>
+            <td style="width:10%; text-align:left;">2h</td>
+            
+            
+                <td style="width:10%; text-align:left;">
+                    
+                </td>
+            
+            
+            <td style="width:40%; vertical-valign:top;">
+            
+                
+                    
+                    
+                        
+                            Luís Miguel Ferreira Nunes
+                            <br>
+                        
+                    
+                
+            </td>	
+            <td style="width:13%; text-align:left;">
+                
+                    
+                        
+                            Ed. Principal - S2.12
+                        
+                    
+                    
+                
+            </td>
+        </tr>
+        
+            
+            
+            
+                <tr>                                                            	
+                    <td colspan="6">	
+                        <table class="tabledefault">
+                            
+                                
+                                    
+                                    
+                                        
+                                            <tbody><tr>
+                                              <td class="label"><p>Português:</p></td>
+                                              <td class="cellcontent"><p>Avaliação: teste.</p></td>
+                                            </tr>
+                                        
+                                            <tr>
+                                              <td class="label"><p>Inglês:</p></td>
+                                              <td class="cellcontent"><p><span>Assessment: test.</span></p></td>
+                                            </tr>
+                                        
+                                                
+                                        
+                            
+                        </tbody></table>
+                    </td>
+                </tr>
+            
+        
+    </tbody></table>
+</td>
+</tr>
+
+<tr>
+<td style="width:100%;">
+    <table class="darkrow" style="width:100%;border-top:0;border-bottom:1px solid #ccc;">
+        <tbody><tr class="cellheader">
+            
+                
+                    <td style="width:8%;text-align:left;"><strong>Turma</strong></td>
+                
+                
+            
+            <td style="width:10%;text-align:left;"><strong>Data</strong></td>												
+            <td style="width:10%;text-align:left;"><strong>Hora de Início</strong></td>
+            <td style="width:10%;text-align:left;"><strong>Duração</strong></td>
+            
+            
+                <td style="width:10%;text-align:left;"><strong>Modo de Leccionação</strong></td>
+            
+            
+            <td style="width:40%;text-align:left;"><strong>Docentes</strong></td>
+            <td style="width:13%;text-align:left;"><strong>Sala</strong></td>
+        </tr>
+        <tr>
+            
+                
+                    <td style="width:8%; text-align:left;">T11</td>
+                
+                
+            
+            
+            <td style="width:10%; text-align:left;">2021-12-06</td>
+            <td style="width:10%; text-align:left;">09:00</td>
+            <td style="width:10%; text-align:left;">2h</td>
+            
+            
+                <td style="width:10%; text-align:left;">
+                    
+                </td>
+            
+            
+            <td style="width:40%; vertical-valign:top;">
+            
+                
+                    
+                    
+                        
+                            Luís Miguel Ferreira Nunes
+                            <br>
+                        
+                    
+                
+            </td>	
+            <td style="width:13%; text-align:left;">
+                
+                    
+                        
+                            Ed. Principal - S2.12
+                        
+                    
+                    
+                
+            </td>
+        </tr>
+        
+            
+            
+            
+                <tr>                                                            	
+                    <td colspan="6">	
+                        <table class="tabledefault">
+                            
+                                
+                                    
+                                    
+                                        
+                                            <tbody><tr>
+                                              <td class="label"><p>Português:</p></td>
+                                              <td class="cellcontent"><p>Avaliação: teste.</p></td>
+                                            </tr>
+                                        
+                                            <tr>
+                                              <td class="label"><p>Inglês:</p></td>
+                                              <td class="cellcontent"><p><span>Assessment: test.</span></p></td>
+                                            </tr>
+                                        
+                                                
+                                        
+                            
+                        </tbody></table>
+                    </td>
+                </tr>
+            
+        
+    </tbody></table>
+</td>
+</tr>
+
+</tbody></table>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody></table>
+                    </td>
+                </tr>
+            </tbody></table>
+        </td>
+    </tr>
+
+<tr>
+    <td>
+        <table class="zone">
+            <tbody><tr>
+                <td>
+                    <table class="horizontalline">
+                        <tbody><tr>
+                            <td class="subtitle">
+                                <label>Turmas</label>
+                            </td>
+                        </tr>									
+                    </tbody></table>
+                </td>		
+            </tr>
+            <tr>
+                <td>
+                    <table class="zonecontent">
+                        <tbody><tr>
+                            <td>
+                                
+<table cellpadding="4" cellspacing="2" style="width: 100%" class="displaytable">
+<thead>
+<tr>
+<th class="cellheaderleft">Inscrito</th>
+<th class="cellheaderleft">Perfil</th>
+<th class="cellheaderleft">Turma</th>
+<th class="cellheaderleft">Docentes</th>
+<th class="cellheader">Aulas Passadas</th>
+<th class="cellheader">Sumários Inseridos</th>
+<th class="cellheader"></th></tr></thead>
+<tbody>
+<tr class="lightrow">
+<td class="contentLeft" style="width: 5%">Sim</td>
+<td class="contentLeft" style="width: 10%">T1</td>
+<td class="contentLeft" style="width: 10%">T11</td>
+<td class="contentLeft" style="width: 42%"><label title="Luís Miguel Ferreira Nunes">Luís Nunes</label><br><label title="Pedro Manuel Espírito Santo">Pedro Espírito Santo</label><br></td>
+<td class="contentCenter" style="width: 10%">22</td>
+<td class="contentCenter" style="width: 10%">22</td>
+<td class="contentCenter"><a class="botaodetalhes" href="listaAulas.do?args=9447377300248239" onclick="javascript: setCookiePosition('positionEdicaoSumarios');">Detalhes</a></td></tr></tbody></table>
+                            </td>
+                        </tr>
+                    </tbody></table>
+                </td>
+            </tr>
+        </tbody></table>
+    </td>
+</tr>
+</tbody></table>
+
+@endsection
