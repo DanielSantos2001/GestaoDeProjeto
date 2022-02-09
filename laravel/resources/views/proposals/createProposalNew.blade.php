e0b18e9189cb0c4a08ae1f3604047a11da5c459d
jpeixe <92375936+jpeixe@users.noreply.github.com>
2022-02-09 16:26:07 +0000

    inclusão do sidemenu
    inclusão do sidemenu nas paginas de detalhes de proposta e criação de propostas

--- laravel/resources/views/proposals/createProposalNew.blade.php
+++ laravel/resources/views/proposals/createProposalNew.blade.php
@@ -3,7 +3,99 @@
 @section('title', 'Criar Proposta de Estágio ')
 
 @section('content')
+  <div id="flxContainer" style="display: block;">
+      <div class="flxContainer" id="areaMenuConteudo">
+          <div class="menu hidden-tablet hidden-phone" style="min-height: 900px;">
+              <div class="menuTitle handMouseCursor" onclick="window.location ='/main'">
+                  <p>Menu</p>
+              </div>
+              <ul class="menuGroup">
 
+                  @if(Session::get('usertype') == 'docente' )
+                  <li class="menuItem">
+                      <span class="menuItemTitle">
+                          Docente
+                      </span>
+                      <ul class="menu-list">
+                          <a href="/main/perfil">
+                              <li class="">
+                                  Informação Pessoal
+                              </li>
+                          </a>
+                          <a href="">
+                              <li class="">
+                                  Acompanhamento de Estágio
+                              </li>
+                          </a>
+                      </ul>
+                  </li>
+
+                  @endif
+
+                  <li class="menuItem">
+                      <span class="menuItemTitle">
+                          Administrador
+                      </span>
+                      <ul class="menu-list">
+                          <a href="">
+                              <li class="">
+                                  Propostas de Estágio
+                              </li>
+                          </a>
+                          <a href="">
+                              <li class="">
+                                  Minhas Propostas
+                              </li>
+                          </a>
+                          <a href="/proposals/createProposal">
+                              <li class="">
+                                  Criar Estágio
+                              </li>
+                          </a>
+                          <a href="">
+                              <li class="">
+                                  Candidatura Estágio
+                              </li>
+                          </a>
+                          <a href="">
+                              <li class="">
+                                  Criar Docente
+                              </li>
+                          </a>
+                          <a href="">
+                              <li class="">
+                                  Criar Não Docente
+                              </li>
+                          </a>
+                          <a href="">
+                              <li class="">
+                                  Registos
+                              </li>
+                          </a>
+                          <a href="">
+                              <li class="">
+                                  InfoDocente
+                              </li>
+                          </a>
+                      </ul>
+                  </li>
+
+                  <div style="position: relative; bottom:0;">
+                      <ul class="menuGroup">
+                          <li class="menuItem">
+                              <ul class="menu-list" style="list-style: none;">
+                                  <li>
+                                      <a onclick="abrirEcraModalPrivacidade();">
+                                          <span class="hand" id="politicaPrivacidade">Política de Privacidade</span>
+                                      </a>
+                                  </li>
+                              </ul>
+                          </li>
+                      </ul>
+                  </div>
+              </ul>
+          </div>
+          <div class="containermain">
 <div class="navtableLight">
     <a href="/main">
         <span id="spanPrimeiroElementoBarraNavegacao" class="darkArrow clickArrow" style="z-index: 100;">
@@ -183,5 +275,7 @@
         </table>
     </form>
 
+</div>        </div>
+    </div>
 </div>
 @endsection
